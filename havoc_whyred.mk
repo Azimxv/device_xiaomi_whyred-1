40f14dd087cc1523c854b9077ccef99417838b15
Kirill Azimov <58362455+Azimxv@users.noreply.github.com>
2020-11-28 23:52:10 +0300

    whyred: coral november fp

--- havoc_whyred.mk
+++ havoc_whyred.mk
@@ -51,8 +51,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.havoc.maintainer=AzimovHero
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="coral-user 11 RP1A.200720.009 6720564 release-keys"
+    PRIVATE_BUILD_DISC="coral-user 11 RP1A.201105.002 6869500 release-keys"
 
 # Build FP to be picked by both system and vendor
-BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys"
-PLATFORM_SECURITY_PATCH_OVERRIDE := 2020-09-05
+BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys"
+PLATFORM_SECURITY_PATCH_OVERRIDE := 2020-11-05
